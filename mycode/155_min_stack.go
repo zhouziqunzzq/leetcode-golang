bb86bcc595c2e5d878e6a21b70de2b6daef8fdb6
Bittersweet <zhouziqun@cool2645.com>
2020-04-10 01:34:10 -0700

    :sparkles: daily challenge 04/10/2020

---
+++ mycode/155_min_stack.go
@@ -0,0 +1,49 @@
+package mycode
+
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{[]int{}, []int{}}
+}
+
+func (this *MinStack) Push(x int) {
+	this.stack = append(this.stack, x)
+	if len(this.minStack) == 0 {
+		this.minStack = append(this.minStack, x)
+	} else {
+		this.minStack = append(this.minStack, min(x, this.minStack[len(this.minStack)-1]))
+	}
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */

fda03704a601d2296e10767950782c12852d4bde
Bittersweet <zhouziqun@cool2645.com>
2020-05-06 10:54:45 -0700

    :sparkles: daily challenge 05/06/2020

---
+++ mycode/169_majority_element.go
@@ -0,0 +1,24 @@
+package mycode
+
+func majorityElement(nums []int) int {
+	n := len(nums)
+	cntMap := make(map[int]int)
+
+	for _, v := range nums {
+		if _, ok := cntMap[v]; ok {
+			cntMap[v]++
+			if cntMap[v] > (n / 2) {
+				return v
+			}
+		} else {
+			cntMap[v] = 1
+		}
+	}
+
+	for _, v := range nums {
+		if cnt, ok := cntMap[v]; ok && cnt > (n / 2) {
+			return v
+		}
+	}
+	return 0
+}

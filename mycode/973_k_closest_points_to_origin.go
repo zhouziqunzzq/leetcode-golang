0d2ddbc1e0d0c2d37540613b6270a059a100a06d
Bittersweet <zhouziqun@cool2645.com>
2020-05-31 01:10:33 -0700

    :bug: fix 973

--- mycode/973_k_closest_points_to_origin.go
+++ mycode/973_k_closest_points_to_origin.go
@@ -3,47 +3,42 @@ package mycode
 // Solution 1: quick select with O(N) avg runtime and O(N^2) worst runtime. <=
 // Solution 2: max-heap with O(N logK) runtime.
 
-import (
-	"math/rand"
-	"time"
-)
-
 func kClosest(points [][]int, K int) [][]int {
-	rand.Seed(time.Now().UnixNano())
-	kClosestQuickSelect(points, K, 0, len(points)-1)
+	l, r := 0, len(points)-1
+
+	for l <= r {
+		mid := kClosestQuickSelect(points, l, r)
+		if mid == K {
+			break
+		} else if mid < K {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+
 	return points[:K]
 }
 
-func kClosestQuickSelect(points [][]int, K, l, r int) {
-	if l > r {
-		return
-	}
+func kClosestQuickSelect(points [][]int, l, r int) int {
+	pivot := make([]int, 2)
+	pivot[0], pivot[1] = points[l][0], points[l][1]
 
-	// partition by pivot
-	pivot := l + rand.Intn(r - l + 1)	// [l, r - l + l + 1)
-	pivotDist := kClosestDist(points[pivot])
-	points[l][0], points[l][1], points[pivot][0], points[pivot][1] =
-		points[pivot][0], points[pivot][1], points[l][0], points[l][1]
-	i := l
-	for j := l+1; j <= r; j++ {
-		if kClosestDist(points[j]) <= pivotDist {
-			i++
-			points[i][0], points[i][1], points[j][0], points[j][1] =
-				points[j][0], points[j][1], points[i][0], points[i][1]
+	// l ... <= pivot ... pivot ... >= pivot ... r
+	for l < r {
+		for l < r && kClosestDist(pivot) <= kClosestDist(points[r]) {
+			r--
 		}
+		points[l][0], points[l][1] = points[r][0], points[r][1]
+		for l < r && kClosestDist(points[l]) <= kClosestDist(pivot) {
+			l++
+		}
+		points[r][0], points[r][1] = points[l][0], points[l][1]
 	}
-	points[l][0], points[l][1], points[i][0], points[i][1] =
-		points[i][0], points[i][1], points[l][0], points[l][1]
-
-	leftLen := i - l + 1
-	if K < leftLen {
-		kClosestQuickSelect(points, K, l, i-1)
-	} else if K > leftLen {
-		kClosestQuickSelect(points, K, i+1, r)
-	}
-	// else K = leftLen and we can just return. The points[:K] would be the answer.
+	points[l][0], points[l][1] = pivot[0], pivot[1]
+	return l
 }
 
-func kClosestDist(p []int) int{
-	return p[0] * p[0] + p[1] * p[1]
+func kClosestDist(p []int) int {
+	return p[0]*p[0] + p[1]*p[1]
 }

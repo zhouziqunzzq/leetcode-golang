e78978e33acf00c6c97f6584e068cb673cfa62fe
Bittersweet <zhouziqun@cool2645.com>
2020-06-18 01:05:01 -0700

    :sparkles: daily challenge 06/18/2020

---
+++ mycode/275_h_index.go
@@ -0,0 +1,19 @@
+package mycode
+
+func hIndex(citations []int) int {
+	N := len(citations)
+	l, r := 0, N
+	mid := 0
+	h := 0
+	for l < r {
+		mid = (l + r) / 2
+		h = N - mid
+		if h <= citations[mid] {	// h is too small, increase h by moving left
+			r = mid
+		} else {	// h is too large, decrease h by moving right
+			l = mid + 1
+		}
+	}
+
+	return N - l
+}

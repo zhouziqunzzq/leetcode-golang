732fda3387c7dafbdb9e99b8c86017b529af0b33
Bittersweet <zhouziqun@cool2645.com>
2020-07-30 00:53:40 -0700

    :sparkles: daily challenge 07/30/2020

---
+++ mycode/140_word_break_ii.go
@@ -0,0 +1,41 @@
+package mycode
+
+import "strings"
+
+func wordBreak(s string, wordDict []string) []string {
+	return wordBreakHelper(s, wordDict, make(map[string][]string))
+}
+
+// memorized DFS
+func wordBreakHelper(s string, wordDict []string, mem map[string][]string) []string {
+	// check memory
+	if rst, ok := mem[s]; ok {
+		return rst
+	}
+
+	rst := make([]string, 0)
+
+	// base case
+	if len(s) == 0 {
+		return append(rst, "")
+	}
+
+	// dfs
+	for _, word := range wordDict {
+		if strings.HasPrefix(s, word) {
+			subRst := wordBreakHelper(s[len(word):], wordDict, mem)
+			for _, sub := range subRst {
+				if len(sub) > 0 {
+					rst = append(rst, word+" "+sub)
+				} else {
+					rst = append(rst, word)
+				}
+			}
+		}
+	}
+
+	// update memory
+	mem[s] = rst
+
+	return rst
+}

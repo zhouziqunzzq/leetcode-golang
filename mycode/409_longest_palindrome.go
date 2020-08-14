60caf9a4430941bdde04a221d99ded729e7f5294
Bittersweet <zhouziqun@cool2645.com>
2020-08-14 01:56:40 -0700

    :sparkles: daily challenge 08/14/2020

---
+++ mycode/409_longest_palindrome.go
@@ -0,0 +1,26 @@
+package mycode
+
+func longestPalindrome(s string) int {
+	var cnt [52]int
+
+	for _, c := range []rune(s) {
+		if c >= 'a' && c <= 'z' {
+			cnt[c-'a']++
+		} else {
+			cnt[c-'A'+26]++
+		}
+	}
+
+	hasOdd := 0
+	rst := 0
+	for _, n := range cnt {
+		if n%2 == 1 {
+			hasOdd = 1
+			n -= 1
+		}
+		rst += n
+	}
+	rst += hasOdd
+
+	return rst
+}

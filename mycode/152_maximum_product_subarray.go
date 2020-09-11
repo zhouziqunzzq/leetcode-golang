576fdd4e2d2e99dc883cc0af21ad2e39e0706096
Bittersweet <zhouziqun@cool2645.com>
2020-09-11 00:28:48 -0700

    :sparkles: daily challenge 09/11/2020

---
+++ mycode/152_maximum_product_subarray.go
@@ -0,0 +1,21 @@
+package mycode
+
+// https://leetcode.com/problems/maximum-product-subarray/discuss/48230/Possibly-simplest-solution-with-O(n)-time-complexity
+func maxProduct(nums []int) int {
+	globalMax := nums[0]
+	curMax, curMin := globalMax, globalMax
+
+	for _, n := range nums[1:] {
+		if n < 0 {
+			lastMax := curMax
+			curMax = maxInt(n, curMin*n)
+			curMin = minInt(n, lastMax*n)
+		} else {
+			curMax = maxInt(n, curMax*n)
+			curMin = minInt(n, curMin*n)
+		}
+		globalMax = maxInt(globalMax, curMax)
+	}
+
+	return globalMax
+}

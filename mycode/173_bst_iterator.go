e4b3dee5a8cfa5416f8aa76565ee9ec66494f8b4
Bittersweet <zhouziqun@cool2645.com>
2020-12-09 01:11:44 -0800

    :sparkles: daily challenge 12/09/2020

---
+++ mycode/173_bst_iterator.go
@@ -0,0 +1,52 @@
+package mycode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// https://leetcode.com/problems/binary-search-tree-iterator/solution/
+// Solution 2: iterative approach using stack
+type BSTIterator struct {
+	s []*TreeNode
+}
+
+func Constructor(root *TreeNode) BSTIterator {
+	it := BSTIterator{
+		s: make([]*TreeNode, 0),
+	}
+	it.goLeft(root)
+	return it
+}
+
+func (this *BSTIterator) goLeft(n *TreeNode) {
+	for n != nil {
+		this.s = append(this.s, n)
+		n = n.Left
+	}
+}
+
+func (this *BSTIterator) Next() int {
+	n := this.s[len(this.s)-1]
+	this.s = this.s[:len(this.s)-1]
+
+	if n.Right != nil {
+		this.goLeft(n.Right)
+	}
+
+	return n.Val
+}
+
+func (this *BSTIterator) HasNext() bool {
+	return len(this.s) > 0
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Next();
+ * param_2 := obj.HasNext();
+ */
--- mycode/382_linked_list_random_node.go
+++ mycode/382_linked_list_random_node.go
@@ -18,7 +18,7 @@ type Solution struct {
 
 /** @param head The linked list's head.
   Note that the head is guaranteed to be not null, so it contains at least one node. */
-func Constructor(head *ListNode) Solution {
+func Constructor382(head *ListNode) Solution {
 	return Solution{
 		head: head,
 	}

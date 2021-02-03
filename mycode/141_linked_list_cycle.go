c201e49167e273058738bbabc1a12e3f46222d3d
Bittersweet <zhouziqun@cool2645.com>
2021-02-03 00:24:10 -0800

    :sparkles: daily challenge 02/03/2021

---
+++ mycode/141_linked_list_cycle.go
@@ -0,0 +1,26 @@
+package mycode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	slow, fast := head, head
+
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}

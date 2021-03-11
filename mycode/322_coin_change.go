5b9f0eef44b010df7b9908a93eb6ef020f8b3246
Bittersweet <zhouziqun@cool2645.com>
2021-03-11 00:53:25 -0800

    :sparkles: daily challenge 03/11/2021

---
+++ mycode/322_coin_change.go
@@ -0,0 +1,23 @@
+package mycode
+
+import "math"
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 0
+
+	for i := 1; i <= amount; i++ {
+		dp[i] = math.MaxInt32
+		for _, c := range coins {
+			if i-c >= 0 {
+				dp[i] = minInt(dp[i], dp[i-c]+1)
+			}
+		}
+	}
+
+	if dp[amount] == math.MaxInt32 {
+		return -1
+	} else {
+		return dp[amount]
+	}
+}
